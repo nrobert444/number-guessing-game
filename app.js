90eb5124293aa47e2d86c1162f9630f87ffac04e
Nicholas Roberto <njr444@gmail.com>
2020-01-14 17:03:44 -0800

    add functions to JS and IDs to html.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const button = document.getElementById('game-button');
 const guessCountSpan = document.getElementById('guess-count')
-let guessCount = 4; 
-
-
+const displayWin = document.getElementById('win-result');
+const displayLose = document.getElementById('lose-result');
+const correctNumber = makeRandom();
+const guess = document.getElementById('enter-guess');
+let guessesLeft = 4; 
 
 
 button.addEventListener('click', () => {
-    guessCount++;
+    guessesLeft--;
+    if (guess > correctNumber) {
+        return "You guessed too high";
+    } else if (guess < correctNumber) {
+        return "You guessed too low";
+    }
+
+
+
 })
+
+
+
+
+
+
+function compareNumbers(guess, correctNumber) {
+    if (guess === correctNumber) return displayWin;
+
+guessCount--;
+
+}
+
+function makeRandom() {
+    return Math.floor(Math.random()) * 10;
+}
+
